4327526a26cb6e9340635bfb6d9ed701d256123b
Ewelina Sadowska <esdvska@gmail.com>
2021-03-02 13:19:25 +0100

    design first-page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { createApi } from "unsplash-js";
+import { GoSearch } from "react-icons/go";
 
-function App() {
+import "./App.css";
+// applicationId: "GG4NHIsZTd4-4UfG3Luk0DAOkg7_NyML_bXfsjnfEJ4",
+// secret: "XtS5mIkqYwP9N1X1ZiUoRzL75Z9JhjgS9-sQ_kJfBEo",
+const App = () => {
+  const [inputSearchValue, setInputSearchValue] = useState("");
+  const api = createApi({
+    accessKey: "GG4NHIsZTd4-4UfG3Luk0DAOkg7_NyML_bXfsjnfEJ4",
+  });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <main>
+        <div className="main-div">
+          <div className="main-wrapper">
+            <h1>Unsplash</h1>
+            <h4>
+              The internet's source of <span>freely-usable images.</span>
+              <br></br>Powered by creators everywhere.
+            </h4>
+            <input
+              type="text"
+              placeholder="Search free high-resolution photos"
+              value={inputSearchValue}
+            ></input>
+            <p>
+              <strong>Trending: </strong>flower, wallpapers, backgrounds, happy,
+              love
+            </p>
+          </div>
+        </div>
+      </main>
     </div>
   );
-}
+};
 
 export default App;
