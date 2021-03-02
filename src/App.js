e14895ff2e952196a785ee88b4b7714b347a43d1
Ewelina Sadowska <esdvska@gmail.com>
2021-03-02 14:40:14 +0100

    input placeholder icon

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
-import React, { useState } from "react";
-import { createApi } from "unsplash-js";
-import { GoSearch } from "react-icons/go";
-
-import "./App.css";
 // applicationId: "GG4NHIsZTd4-4UfG3Luk0DAOkg7_NyML_bXfsjnfEJ4",
 // secret: "XtS5mIkqYwP9N1X1ZiUoRzL75Z9JhjgS9-sQ_kJfBEo",
+
+import React, { useState } from "react";
+import { createApi } from "unsplash-js";
+import "font-awesome/css/font-awesome.min.css";
+
+import "./App.css";
+
 const App = () => {
   const [inputSearchValue, setInputSearchValue] = useState("");
+  const searchPhrase = () => {};
   const api = createApi({
     accessKey: "GG4NHIsZTd4-4UfG3Luk0DAOkg7_NyML_bXfsjnfEJ4",
   });
+
+  const textSearch = "Search free high-resolution photos";
+
   return (
     <div className="app">
       <main>
@@ -20,10 +26,12 @@ const App = () => {
               The internet's source of <span>freely-usable images.</span>
               <br></br>Powered by creators everywhere.
             </h4>
+
             <input
               type="text"
-              placeholder="Search free high-resolution photos"
+              placeholder="   &#xf002;   Search free high-resolution photos"
               value={inputSearchValue}
+              onChange={searchPhrase}
             ></input>
             <p>
               <strong>Trending: </strong>flower, wallpapers, backgrounds, happy,
