f412de69f3a9fa8f82a290c9297e4f933bd41039
Ewelina Sadowska <esdvska@gmail.com>
2021-03-03 10:41:24 +0100

    async api fetch

--- src/App.js
+++ src/App.js
@@ -11,64 +11,76 @@ import TextUnderInput from "./components/TextUnderInput";
 import "font-awesome/css/font-awesome.min.css";
 
 import "./App.css";
+import SearchResults from "./components/SearchResults";
 
 const App = () => {
   const [inputSearchValue, setInputSearchValue] = useState("");
-
   const [testArray, setTestArray] = useState([]);
+  const [searchChange, setSearchChange] = useState(false);
 
   const api = createApi({
     accessKey: "GG4NHIsZTd4-4UfG3Luk0DAOkg7_NyML_bXfsjnfEJ4",
   });
 
-  useEffect(() => {
-    api.search
+  const fetchApi = async () => {
+    if (inputSearchValue.length > 3) {
+      api.search
+        .getPhotos({ query: inputSearchValue })
+        .then((result) => {
+          if (result.length !== 0) {
+            setTestArray(result.response.results);
+            console.log(result.response);
+          }
+        })
+        .catch(() => {
+          console.log("something went wrong!");
+        });
+    }
+  };
 
-      .getPhotos({ query: inputSearchValue })
-      .then((result) => {
-        setTestArray(result.response.results);
-        console.log(result.response);
-      })
-      .catch(() => {
-        console.log("something went wrong!");
-      });
+  useEffect(() => {
+    fetchApi();
   }, [inputSearchValue]);
 
   const searchPhrase = (e) => {
     setInputSearchValue(e.target.value);
   };
-  const handleEnter = () => {
-    console.log("działąm");
+  const handleEnter = (e) => {
+    if (e.key === "Enter") setSearchChange(true);
   };
   return (
     <div className="app">
-      <main>
-        <div className="main-div">
-          <div className="main-wrapper">
-            <h1>Unsplash</h1>
-            <h4>
-              The internet's source of <span>freely-usable images.</span>
-              <br></br>Powered by creators everywhere.
-            </h4>
-            <div className="div-input">
-              {" "}
-              &#xf002;
-              <input
-                type="text"
-                placeholder="Search free high-resolution photos"
-                value={inputSearchValue}
-                onChange={searchPhrase}
-                onKeyDown={handleEnter}
-              ></input>
+      {searchChange && testArray.length !== 0 ? (
+        <SearchResults />
+      ) : (
+        <main>
+          <div className="main-div">
+            <div className="main-wrapper">
+              <h1>Unsplash</h1>
+              <h4>
+                The internet's source of <span>freely-usable images.</span>
+                <br></br>Powered by creators everywhere.
+              </h4>
+              <div className="div-input">
+                {" "}
+                &#xf002;
+                <input
+                  type="text"
+                  placeholder="Search free high-resolution photos"
+                  value={inputSearchValue}
+                  onChange={searchPhrase}
+                  onKeyDown={handleEnter}
+                ></input>
+              </div>
+              {inputSearchValue.length > 3 ? (
+                <SearchArea test={testArray} />
+              ) : (
+                <TextUnderInput />
+              )}
             </div>
-            {inputSearchValue.length > 3 ? (
-              <SearchArea test={testArray} />
-            ) : (
-              <TextUnderInput />
-            )}
           </div>
-        </div>
-      </main>
+        </main>
+      )}
     </div>
   );
 };
---
+++ src/components/SearchResults.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const SearchResults = () => {
+  return (
+    <div>
+      <h1>Island</h1>
+    </div>
+  );
+};
+export default SearchResults;
